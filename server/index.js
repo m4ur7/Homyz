ada077b15110e528dcc98b8dbf033df6d7118051
Mauricio Orellana <mauricioeorellana@gmail.com>
2024-03-07 08:27:32 +0100

    index.js updated

--- server/index.js
+++ server/index.js
@@ -10,13 +10,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors())
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);
 });
 
-app.use('/api/user', userRoute)
-app.use("/api/residency", residencyRoute)
+app.use('/api/user', userRoute);
+app.use('/api/residency', residencyRoute);
